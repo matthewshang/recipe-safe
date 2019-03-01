8435e8ea3b0b9e8133d871aeb19e03c339024b5f
Matthew Shang <mshang47@gmail.com>
2019-03-01 12:36:09 -0800

    Started grid card layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,25 @@
   <div>
     <h1>{{ page_title }}</h1>
 
-    <div v-for="(entry, index) in entries" :key="entry.slug + '_' + index">
-      <router-link :to="'/entries/' + entry.slug">
-        <h2>{{ entry.name }}</h2>
-      </router-link>
-    </div>
+    <b-container class="home-grid">
+      <b-row>
+        <b-col
+          v-for="(entry, index) in entries"
+          :key="entry.slug + '_' + index"
+          class="col-xs-4"
+        >
+          <b-card
+            :title="entry.name"
+            class="home-grid-card"
+          >
+            <b-card-text>
+              Some quick example
+            </b-card-text>
+
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -24,16 +38,24 @@ export default {
         {
           name: "Dumplings",
           slug: "dumplings"
+        },
+        {
+          name: "Pizza",
+          slug: "pizza"
         }
       ]
-    }
+    };
   },
   methods: {
-    goBack () {
-      window.history.length > 1
-        ? this.$router.go(-1)
-        : this.$router.push('/')
-    },
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
+    }
   }
 };
 </script>
+
+<style>
+.home-grid-card {
+  text-align: center;
+}
+</style>
