9d8a943ee080f005ab4e155d7e5dbb40236f2f70
Matthew Shang <mshang47@gmail.com>
2019-03-06 12:13:05 -0800

    Added home search box and button

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <b-container class="home-grid">
-      <b-row v-for="i in rowCount" :key="i" no-gutter> 
+    <b-container id="top-bar-container" class="my-3 mx-auto d-flex">
+      <b-form-input
+        v-model="query"
+        type="search"
+        placeholder="Search for a recipe"
+        id="top-search-box"
+        class="mr-2"
+      />
+      <b-button id="new-recipe-button">+</b-button>
+    </b-container>
+    <b-container>
+      <b-row v-for="i in rowCount" :key="i" no-gutter>
         <b-col
           v-for="(entry, index) in entries.slice((i - 1) * itemsPerRow, i * itemsPerRow)"
           :key="entry.slug + '_' + index"
@@ -15,30 +25,29 @@
 </template>
 
 <script>
-import Api from '../api.js'
-import HomeRecipeCard from '../components/HomeRecipeCard.vue'
+import Api from "../api.js";
+import HomeRecipeCard from "../components/HomeRecipeCard.vue";
 
 export default {
   name: "Home",
   components: {
-    'home-recipe-card': HomeRecipeCard
+    "home-recipe-card": HomeRecipeCard
   },
   data() {
     return {
       page_title: "Home",
       entries: null,
-      itemsPerRow: 3
-    }
+      itemsPerRow: 3,
+      query: ""
+    };
   },
-  created () {
-    Api
-      .get('entries')
-      .then(res => (this.entries = res.data))
+  created() {
+    Api.get("entries").then(res => (this.entries = res.data));
   },
   computed: {
     rowCount() {
-      if (this.entries === null) return 0
-      return Math.ceil(this.entries.length / this.itemsPerRow)
+      if (this.entries === null) return 0;
+      return Math.ceil(this.entries.length / this.itemsPerRow);
     }
   },
   methods: {
@@ -51,6 +60,17 @@ export default {
 
 <style>
 .grid-cols {
-  max-width:33%;
+  max-width: 33%;
+}
+
+#top-search-box {
+  width: 50%;
+}
+
+#top-bar-container {
+}
+
+#new-recipe-button {
+  margin-left: auto;
 }
 </style>
