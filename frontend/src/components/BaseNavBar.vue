1de85075da2d38f1aa62399c403f16ab71848667
Matthew Shang <mshang47@gmail.com>
2019-03-17 18:57:16 -0700

    Remove Import view and user from navbar

--- frontend/src/components/BaseNavBar.vue
+++ frontend/src/components/BaseNavBar.vue
@@ -3,14 +3,6 @@
     <b-navbar-brand :to="{ path: '/' }">RecipeSafe</b-navbar-brand>
     <b-navbar-nav>
       <b-nav-item :to="{ path: '/' }">Home</b-nav-item>
-      <b-nav-item :to="{ path: '/import' }">Import</b-nav-item>
-    </b-navbar-nav>
-
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item-dropdown text="User" right>
-        <b-dropdown-item :to="{ path: '/notfound' }">Account</b-dropdown-item>
-        <b-dropdown-item :to="{ path: '/notfound' }">Logout</b-dropdown-item>
-      </b-nav-item-dropdown>
     </b-navbar-nav>
   </b-navbar>
 </template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Entry from './views/Entry.vue'
-import Import from './views/Import.vue'
 import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
@@ -15,11 +14,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/import',
-      name: 'Import',
-      component: Import
     },
     {
       path: '/entries/:slug',
